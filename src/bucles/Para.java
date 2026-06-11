1d896fc50bd4b684076a1f3fbe7b0ebc2bdb6587
Juan Quiroz <e06731030@gmail.com>
2026-06-11 17:14:01 -0500

    realizando un ejercicio con el (PARA)

---
+++ src/bucles/Para.java
@@ -0,0 +1,21 @@
+package bucles;
+
+public class Para {
+    public static void main(String[] args) {
+        /*
+        SINTANXIS DE CICLO FOR = PARA
+        for (2: inicio; 1: condicion; 3: incremento o actualizacion){
+        bloques de instruciones a realizar o acciones
+        }
+         */
+
+        // crear un algoritmo que permita imprimir los numeros del 1 al 10
+        for (int valorInicial = 0; valorInicial <= 10; valorInicial ++){
+            System.out.println("6 x "+ valorInicial + " = " + (6 * valorInicial));
+        }
+
+
+
+
+    }
+}
